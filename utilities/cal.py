0caccaa0e8b29132b44ba5db00aa2439f63a025c
Warutchot <116997087+ChunSensei@users.noreply.github.com>
2025-07-21 15:36:39 +0700

    Add files via upload

---
+++ utilities/cal.py
@@ -0,0 +1,14 @@
+def name (num1,num2):
+    return num1 + num2
+
+
+def substract (a,b):
+    return a - b
+
+def multiply (num1,num2):
+    return num1 * num2
+
+def divide (num1,num2):
+    return num1 / num2
+
+
